95e13ec7f9e41ee5de23a9b6496a71c7b2db36eb
EgorK1ST <fokine504@gmail.com>
2026-01-31 16:42:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from bot_logic import plastic_craft
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot("8592671464:AAE23k63V-ce-Y2R6JOAUKN4kZ1q8k2Zj0s")
 
 # Команда /start
 @bot.message_handler(commands=["start"])
@@ -10,7 +10,6 @@ def send_start(message):
 
 @bot.message_handler(commands=['plastic_crafts'])
 def send_plastic_crafts(message):
-    bot.reply_to(message, '')
-
+    bot.reply_to(message, f'Конечно! Вот ваша поделка: {plastic_craft()}')
 
 bot.polling()
